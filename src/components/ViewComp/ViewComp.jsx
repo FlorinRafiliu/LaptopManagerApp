1dc34ea83bd4b3bb839f0300ed9f7598e49a62b3
916-Rafiliu-Florin <146828716+916-Rafiliu-Florin@users.noreply.github.com>
2025-03-26 20:55:09 +0200

    add pagination

--- src/components/ViewComp/ViewComp.jsx
+++ src/components/ViewComp/ViewComp.jsx
@@ -13,9 +13,19 @@ import { useState, useEffect } from "react";
 function ViewComp({appCallBack}) {
 
     const [elem, setElem] = useState(data.map(e => <CardComp id={e.id} editCall={appCallBack} deleteCall={editElem} />));
+    const [currentPage, setPage] = useState(0);
 
     function editElem(newData) {
         setElem(newData.map(e => <CardComp id={e.id} editCall={appCallBack} deleteCall={editElem} />))
+    }
+
+    function updatePage(val) {
+        if(val + currentPage < 0)
+            return;
+        if(val + currentPage > Math.floor(elem.length / 6))
+            return;
+
+        setPage(prev => prev + val);
     }
 
     return (
@@ -23,9 +33,16 @@ function ViewComp({appCallBack}) {
             <div className={styles.mainBack}>
                 <FilterComp callBack={editElem}/>
                 <div className={styles.grid}>
-                    {elem}
+                    {elem.slice(currentPage * 6, (currentPage + 1) * 6)}
                 </div>
             </div>
+
+            <div className={styles.paginare}>
+                <button type="button" onClick={() => updatePage(-1)}>Previous</button>
+                <div>{currentPage + 1}</div>
+                <button type="button" onClick={() => updatePage(1)}>Next</button>
+            </div>
+
             <div className={styles.charts}>
                 <div><PieChart /></div>
                 <div><LineGraph /></div>
--- src/data.js
+++ src/data.js
@@ -12,13 +12,13 @@ export const data = [
     ,
     {
         id: 1,
-        name: "Macbook Air 12",
-        brand: "Apple",
-        category: "ultrabook",
-        description: "Al doilea cel mai tare laptop din lume",
-        year: 2023,
-        price: 3000,
-        path: 'macbook.jpg',
+        name: "ACER Aspire 3",
+        brand: "Acer",
+        category: "business",
+        description: "Alege Aspire 3 cu cele mai recente procesoare AMD Ryzen Series create pentru multitasking si productivitate. Usor de utilizat, acest laptop este potrivit pentru orice activitate.",
+        year: 2024,
+        price: 1899,
+        path: 'acer.jpg',
     }
     ,
     {
@@ -41,5 +41,104 @@ export const data = [
         year: 2022,
         price: 2400,
         path: 'asus.jpg',
+    }
+    ,
+    {
+        id: 4,
+        name: "Macbook Air 13",
+        brand: "Apple",
+        category: "ultrabook",
+        description: "Cel mai tare laptop din lume",
+        year: 2025,
+        price: 4500,
+        path: 'apple.jpg',
+    }
+    ,
+    {
+        id: 5,
+        name: "Macbook Air 12",
+        brand: "Apple",
+        category: "ultrabook",
+        description: "Al doilea cel mai tare laptop din lume",
+        year: 2023,
+        price: 3000,
+        path: 'macbook.jpg',
+    }
+    ,
+    {
+        id: 6,
+        name: "Lenovo Legion 16",
+        brand: "Lenovo",
+        category: "gaming",
+        description: "Al treilea cel mai tare laptop din lume",
+        year: 2023,
+        price: 1200,
+        path: 'lenovo.jpg',
+    }
+    ,
+    {
+        id: 7,
+        name: "Asus Zenbook 15",
+        brand: "Asus",
+        category: "business",
+        description: "Al patrulea cel mai tare laptop din lume",
+        year: 2022,
+        price: 2400,
+        path: 'asus.jpg',
+    }
+    ,
+    {
+        id: 8,
+        name: "MSI Cyborg",
+        brand: "MSI",
+        category: "gaming",
+        description: "Urmatoarea faza a evolutiei este aici: scufunda-te in lumea jocurilor in stil cyberpunk, depaseste-ti adversarul cu un hardware revolutionar. Incorporand un design exterior mecanizat futurist, Cyborg 15 A13V este construit pentru a fi cel mai revolutionar laptop de gaming.",
+        year: 2024,
+        price: 2199,
+        path: 'msi.jpg',
+    }
+    ,
+    {
+        id: 9,
+        name: "Macbook Air 12",
+        brand: "Apple",
+        category: "ultrabook",
+        description: "Al doilea cel mai tare laptop din lume",
+        year: 2023,
+        price: 3000,
+        path: 'macbook.jpg',
+    }
+    ,
+    {
+        id: 10,
+        name: "Lenovo Legion 16",
+        brand: "Lenovo",
+        category: "gaming",
+        description: "Al treilea cel mai tare laptop din lume",
+        year: 2023,
+        price: 1200,
+        path: 'lenovo.jpg',
+    }
+    ,
+    {
+        id: 11,
+        name: "Asus Zenbook 15",
+        brand: "Asus",
+        category: "business",
+        description: "Al patrulea cel mai tare laptop din lume",
+        year: 2022,
+        price: 2400,
+        path: 'asus.jpg',
+    }
+    ,
+    {
+        id: 12,
+        name: "Lenovo Legion 16",
+        brand: "Lenovo",
+        category: "gaming",
+        description: "Al treilea cel mai tare laptop din lume",
+        year: 2023,
+        price: 1200,
+        path: 'lenovo.jpg',
     }
 ];
